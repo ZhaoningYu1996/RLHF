c5b81c1f5ecd64641fdbd4e287cbdf4ffb85cf66
ZhaoningYu1996 <zha0nin9@gmail.com>
2025-03-26 23:33:29 -0500

    change config

--- unsloth_sft.py
+++ unsloth_sft.py
@@ -4,7 +4,7 @@ max_seq_length = 1024 # Can increase for longer reasoning traces
 lora_rank = 32 # Larger rank = smarter, but slower
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "meta-llama/Llama-3.2-1B",
+    model_name = "meta-llama/Llama-3.2-3B-Instruct",
     max_seq_length = max_seq_length,
     load_in_4bit = True, # False for LoRA 16bit
     fast_inference = True, # Enable vLLM fast inference
@@ -23,3 +23,100 @@ model = FastLanguageModel.get_peft_model(
     use_gradient_checkpointing = "unsloth", # Enable long context finetuning
     random_state = 3407,
 )
+
+# import ipdb; ipdb.set_trace()
+
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
+def formatting_prompts_func(examples):
+    instructions = examples["instruction"]
+    inputs       = examples["input"]
+    outputs      = examples["output"]
+    texts = []
+    for instruction, input, output in zip(instructions, inputs, outputs):
+        # Must add EOS_TOKEN, otherwise your generation will go on forever!
+        text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
+        texts.append(text)
+    return { "text" : texts, }
+pass
+
+from datasets import load_dataset
+dataset = load_dataset("json", data_files="sft_data/zinc/zinc.json", split="train")
+print(dataset)
+dataset = dataset.map(formatting_prompts_func, batched = True,)
+
+
+# Train the model
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from unsloth import is_bfloat16_supported
+
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    dataset_num_proc = 2,
+    packing = False, # Can make training 5x faster for short sequences.
+    args = TrainingArguments(
+        per_device_train_batch_size = 2,
+        gradient_accumulation_steps = 4,
+        warmup_steps = 5,
+        # num_train_epochs = 1, # Set this for 1 full training run.
+        max_steps = 60,
+        learning_rate = 2e-4,
+        fp16 = not is_bfloat16_supported(),
+        bf16 = is_bfloat16_supported(),
+        logging_steps = 1,
+        optim = "adamw_8bit",
+        weight_decay = 0.01,
+        lr_scheduler_type = "linear",
+        seed = 3407,
+        output_dir = "outputs",
+        report_to = "none", # Use this for WandB etc
+    ),
+)
+trainer_stats = trainer.train()
+
+# Save the model
+model.save_pretrained("saved_models/Llama3_2_3B_lora_model")  # Local saving
+tokenizer.save_pretrained("saved_models/Llama3_2_3B_lora_model")
+
+# Evaluate the original model
+FastLanguageModel.for_inference(model)
+
+inputs = tokenizer(
+[
+    alpaca_prompt.format(
+        "What is a famous tall tower in Paris?", # instruction
+        "", # input
+        "", # output - leave this blank for generation!
+    )
+], return_tensors = "pt").to("cuda")
+
+from transformers import TextStreamer
+text_streamer = TextStreamer(tokenizer)
+_ = model.generate(**inputs, streamer = text_streamer, max_new_tokens = 128)
+
+# Evaluate the finetuned model
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "lora_model", # YOUR MODEL YOU USED FOR TRAINING
+    max_seq_length = max_seq_length,
+    dtype = None,
+    load_in_4bit = True,
+)
+FastLanguageModel.for_inference(model)
+
+text_streamer = TextStreamer(tokenizer)
+_ = model.generate(**inputs, streamer = text_streamer, max_new_tokens = 128)
